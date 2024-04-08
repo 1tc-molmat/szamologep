cad2a9b6f2339f4a36644586a9ff2fecb0383f88
1tc-molmat <131852015+1tc-molmat@users.noreply.github.com>
2024-04-08 12:11:15 +0200

    add

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from pyscript import document
 import random
 
-for _ in range(5):
-    szam=random.randint(1,10)
-    document.querySelector("#kimenet").innerText+=str(szam)+"\n"
+#for _ in range(5):
+ #   szam=random.randint(1,10)
+   # document.querySelector("#kimenet").innerText+=str(szam)+"\n"
     
-def beolvas(event):
-    felhasznalo = document.querySelector("#beviteli-mezo").value
-    document.querySelector("#kimenet1").innerText = felhasznalo
+#def beolvas(event):
+ #   felhasznalo = document.querySelector("#beviteli-mezo").value
+  #  document.querySelector("#kimenet1").innerText = felhasznalo
+
+
+def kivonas(event):
+    eredmeny=document.getElementById("beviteli1").value + document.getElementById("beviteli2").value
